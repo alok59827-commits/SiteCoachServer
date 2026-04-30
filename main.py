26915eb5eee108f9d6fd1f7a3087be73326d7881
alok59827-commits <alok59827@gmail.com>
2026-04-30 10:22:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def analyze_audio(file: UploadFile = File(...)):
             model="llama-3.1-8b-instant",
             response_format={"type": "json_object"},
             temperature=0.0,
-            max_tokens=1024
+            max_tokens=4096
         )
         
         result_json = json.loads(chat_completion.choices[0].message.content)
